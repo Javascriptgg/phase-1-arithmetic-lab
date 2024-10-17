ad307d53f3e19534bb9da0f843212c4f0b215430
David Muchahi <Muchahi853@gmail.com>
2024-10-17 15:02:31 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// Write your code
+const num1=31;
+const num2=2;
+const multiply=31*2;
+const random=Math.round(Math.random()*10)+1;
+const num3=8;
+const num4=2;
+const mod=num3/num4;
+const max=Math.max(1,18,17,16,20);
